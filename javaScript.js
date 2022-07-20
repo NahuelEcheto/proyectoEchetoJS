ea78ebc92d77425d652fd34ec26f983806709d87
nahuel echeto <echetonahuel@gmail.com>
2022-07-20 20:18:06 -0300

    array de clientes

---
+++ javaScript.js
@@ -0,0 +1,10 @@
+let clientes = [];
+
+let nuevoCliente = clientes.push(["Nahuel", "Echeto", 23, "Pehuajo", "Buenos Aires"]);
+let nuevoCliente1 = clientes.push(["Micael", "Papa", 23, "Mar del Plata", "Buenos Aires"]);
+let nuevoCliente2 = clientes.unshift(["Paola", "Sanchez", 25, "La Plata", "Buenos Aires"]);
+
+
+console.log(clientes);
+
+
