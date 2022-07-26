9937808971e7b1c26e9dd5129b7f4e83c85ba494
nahuel echeto <echetonahuel@gmail.com>
2022-07-25 22:37:34 -0300

    primer entrega proyecto final

--- javaScript.js
+++ javaScript.js
@@ -1,10 +1,74 @@
-let clientes = [];
+let nombreCliente = prompt("Ingresa tu nombre");
+let apellidoCliente = prompt("Ingresa tu apellido");
+let direccionCliente = prompt("Ingresa la dirección a donde enviar el pedido");
 
-let nuevoCliente = clientes.push(["Nahuel", "Echeto", 23, "Pehuajo", "Buenos Aires"]);
-let nuevoCliente1 = clientes.push(["Micael", "Papa", 23, "Mar del Plata", "Buenos Aires"]);
-let nuevoCliente2 = clientes.unshift(["Paola", "Sanchez", 25, "La Plata", "Buenos Aires"]);
+const cliente = [
+    {
+    nombre: nombreCliente,
+    apellido: apellidoCliente,
+    direccion: direccionCliente
+},
+]
+
+console.log(cliente);
 
 
-console.log(clientes);
+const hamburguesa = [
+    {
+        producto: "Hamburguesa clásica",
+        precio: 300,
+    },
+    {
+        producto: "Hamburguesa con queso",
+        precio: 370,
+    },
+    {
+        producto: "Hamburguesa doble con cheddar",
+        precio: 670,
+    },
+    {
+        producto: "Hamburguesa con bacon",
+        precio: 450,
+    },
+    {
+        producto: "Hamburguesa triple con queso",
+        precio: 790,
+    },
+    {
+        producto: "Hamburguesa con guacamole",
+        precio: 460,
+    },
+    {
+        producto: "Hamburguesa de pollo",
+        precio: 330,
+    },
+    {
+        producto: "Hamburguesa de soja",
+        precio: 390,
+    },
+    {
+        producto: "Hamburguesa de lentejas",
+        precio: 360,
+    },
+]
+
+const filtroDePrecio = hamburguesa.filter((curr) => {
+    return curr.precio < 500
+})
+
+console.log(filtroDePrecio);
+
+const ordenDePrecios = hamburguesa.sort((a, b) => {
+    if(a.precio > b.precio){
+        return 1
+    }
+    else if(a.precio < b.precio){
+        return -1
+    }
+})
+
+console.log(ordenDePrecios);
+
+
 
 
